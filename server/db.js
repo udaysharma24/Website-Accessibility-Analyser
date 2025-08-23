3b32144786d36509a18ecde1b14abfdaaf05c7fd
udaysharma24 <udaysanjeev2004@gmail.com>
2025-08-23 17:41:56 +0530

    Use environment variables for PostgreSQL

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 import {Pool} from "pg"
 
 const pool= new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'intelliaccess_auth',
-    password: 'uday@24022004',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 })
 
 async function conn(){
