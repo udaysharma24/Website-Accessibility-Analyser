b4fcf1a2014910423bc294b3469d0aef7dadcd8c
udaysharma24 <udaysanjeev2004@gmail.com>
2025-08-23 18:22:11 +0530

    Fixed some changes related to tailwind packages, and Node_env

--- server/db.js
+++ server/db.js
@@ -1,14 +1,16 @@
 import {Pool} from "pg"
 
+const isProduction = process.env.NODE_ENV === "production"
+
 const pool= new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false, 
-    }
+    ssl: isProduction
+    ? { rejectUnauthorized: false } 
+    : false,
 })
 
 async function conn(){
