cdff88a38ff3635d3d24b0a0893869f8f62c1d09
udaysharma24 <udaysanjeev2004@gmail.com>
2025-08-26 12:40:54 +0530

    Fixed logo issue on landing page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -16,17 +16,18 @@ export default function Home() {
 
   return (
     <div
-      className={`bg-[url('/analytics-bg.jpg')] min-h-screen bg-cover bg-center flex flex-col ${poppins.className}`}
+      className={`relative bg-[url('/analytics-bg.jpg')] min-h-screen bg-cover bg-center flex flex-col ${poppins.className}`}
     >
-      <img
-        className="h-16 sm:h-20 md:h-24 lg:h-28 xl:h-32 ml-2 sm:ml-4 mt-2 sm:mt-4"
-        src="/IAlogo_dark.png"
-        alt="logo"
-      />
+      <div className="absolute top-4 left-4">
+        <img
+          className="h-12 sm:h-16 md:h-20 lg:h-24 xl:h-28 w-auto"
+          src="/IAlogo_dark.png"
+          alt="logo"
+        />
+      </div>
 
-      {/* Heading */}
       <div className="flex flex-col justify-center flex-1 px-4 sm:px-8 md:px-16 lg:px-24">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-amber-600 drop-shadow-[0_0_2px_black] leading-snug">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl text-amber-600 drop-shadow-[0_0_2px_black] leading-snug">
           Build inclusive websites effortlessly —
         </h1>
         <h1 className="text-base sm:text-lg md:text-2xl lg:text-3xl text-white mt-3 drop-shadow-[0_0_2px_black] max-w-4xl">
@@ -34,7 +35,6 @@ export default function Home() {
           click.
         </h1>
 
-        {/* Button */}
         <button
           className="bg-violet-800 hover:bg-violet-950 px-5 py-2 sm:px-6 sm:py-3 md:px-8 md:py-3 mt-6 sm:mt-8 md:mt-10 w-fit rounded-xl cursor-pointer border-2 border-white text-white text-sm sm:text-base md:text-lg drop-shadow-[0_0_3px_white]"
           onClick={handleclick}
