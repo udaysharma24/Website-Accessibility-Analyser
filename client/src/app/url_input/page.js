9c8aa1b4f603387a627484f16daf90c004273bad
udaysharma24 <udaysanjeev2004@gmail.com>
2025-08-26 23:02:46 +0530

    Name issue-2

--- client/src/app/url_input/page.js
+++ client/src/app/url_input/page.js
@@ -1,14 +1,22 @@
 "use client"
-import { useEffect, useState } from "react"
+import { Suspense } from "react"
 import VantaBirds from "@/components/VantaBirds"
 import { useSearchParams } from 'next/navigation';
 
-export default function Url_input(){
+function UrlInputContent() {
     const searchParams = useSearchParams();
     const username = searchParams.get('username');
     return(
         <main className="">
             <VantaBirds username={username} />
         </main>
+    )
+}
+
+export default function Url_input() {
+    return (
+        <Suspense>
+            <UrlInputContent />
+        </Suspense>
     )
 }
--- client/src/components/VantaBirds.js
+++ client/src/components/VantaBirds.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { Poppins } from 'next/font/google';
 
 
