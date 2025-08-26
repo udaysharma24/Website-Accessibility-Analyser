8f0189b533061c9bb62fc6ec500f899b006ccba9
udaysharma24 <udaysanjeev2004@gmail.com>
2025-08-26 17:58:56 +0530

    Untrack .env and update .gitignore

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ import cookieParser from "cookie-parser"
 
 dotenv.config()
 
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.FRONTEND_URL];
 
 const PgSession= connect_pg(session)
 
